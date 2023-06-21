57a6005699d6369bba085b946cbe0cac893ffbee
Taylor Swanson <taylor.swanson@elastic.co>
2023-06-21 13:14:01 -0500

    Review comments

--- fleet_output/main.tf
+++ fleet_output/main.tf
@@ -2,7 +2,9 @@ resource "elasticstack_fleet_output" "elasticsearch_output" {
   name = var.name
   type = "elasticsearch"
   config_yaml = yamlencode({
-    "ssl.verification_mode" : var.insecure ? "none" : "certificate"
+    ssl = {
+      verification_mode = var.insecure ? "none" : "certificate"
+    }
   })
   default_integrations = false
   default_monitoring   = false
