04eb067dc6129f8e81dc0731af8e8c278111f8cd
Andrew Kroh <andrew.kroh@elastic.co>
2024-03-28 11:12:16 -0400

    Add elasticstack_fleet_integration to fleet_package_policy (#32)
    Control version of the installed package by using elasticstack_fleet_integration.
    
    Previously the version was honored only the first time that a package was installed.
    With this change now the package version will be upgraded.

--- fleet_package_policy/providers.tf
+++ fleet_package_policy/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    elasticstack = {
+      source  = "elastic/elasticstack"
+      version = ">= 0.11.0"
+    }
     restapi = {
       source  = "Mastercard/restapi"
       version = ">= 1.18.0"
